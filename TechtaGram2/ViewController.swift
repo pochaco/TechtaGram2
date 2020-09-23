23b71813827aeb8a7622ac15478d7d1ccc5b76ff
Miu Yamamoto <mi.oct09@gmail.com>
2020-09-23 19:20:12 +0900

    Initial Commit

---
+++ TechtaGram2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechtaGram2
+//
+//  Created by Yamamoto Miu on 2020/09/23.
+//  Copyright © 2020 Yamamoto Miu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
